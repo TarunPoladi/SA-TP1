32adf1554612345daec384cbf210fe607d9923cb
Bryan Oliveira <vicdaruf@yahoo.com>
2016-02-25 16:59:41 +0000

    Added SortFilter to SystemA folder

--- System A/SortFilter.java
+++ System A/SortFilter.java
@@ -30,14 +30,20 @@ public class SortFilter extends FilterFramework
 	public void run()
     {
         Calendar TimeStamp = Calendar.getInstance();
+
+        ArrayList<Calendar> TimeStampList = new ArrayList<>();
+
         SimpleDateFormat TimeStampFormat = new SimpleDateFormat("yyyy MM dd::hh:mm:ss:SSS");
 
+        int MeasurementLength = 8;		// This is the length of all measurements (including time) in bytes
         int IdLength = 4;               // This is the length of IDs in the byte stream
 
         int bytesread = 0;                  // Number of bytes read from the input file.
         int byteswritten = 0;               // Number of bytes written to the stream.
         byte databyte = 0;                  // The byte of data read from the file
 
+        long measurement;				// This is the word used to store all measurements - conversions are illustrated.
+        int i;
         int id;                         // This is the measurement id
 
         // Next we write a message to the terminal to let the world know we are alive...
@@ -56,7 +62,7 @@ public class SortFilter extends FilterFramework
 
                 for (i=0; i<IdLength; i++ )
                 {
-                    databyte = ReadFilterInputPort();   // This is where we read the byte from the stream...
+                    databyte = ReadFilterInputPort(0);   // This is where we read the byte from the stream...
 
                     id = id | (databyte & 0xFF);        // We append the byte on to ID...
 
@@ -70,9 +76,29 @@ public class SortFilter extends FilterFramework
 
                 } // for
 
+                // measurment
+                measurement = 0;
+
+                for (i=0; i<MeasurementLength; i++ )
+                {
+                    databyte = ReadFilterInputPort(0);
+                    measurement = measurement | (databyte & 0xFF);  // We append the byte on to measurement...
+
+                    if (i != MeasurementLength-1)                   // If this is not the last byte, then slide the
+                    {                                               // previously appended byte to the left by one byte
+                        measurement = measurement << 8;             // to make room for the next byte we append to the
+                                                                    // measurement
+                    } // if
+
+                    bytesread++;                                    // Increment the byte count
+
+                } // if
+
+                // time
                 if ( id == 0 )
                 {
-                    TimeStamp.setTimeInMillis(measurement);
+                	// Add timestamp to ArrayList
+                    //TimeStampList.add( TimeStamp.setTimeInMillis(measurement) );
 
                 } // if
 
@@ -89,10 +115,11 @@ public class SortFilter extends FilterFramework
         } // while
 
         // Sort
+        Collections.sort(TimeStampList);
 
-
+        
         //Send
-        WriteFilterOutputPort(databyte);
+        WriteFilterOutputPort(databyte,0);
         byteswritten++;
 
    } // run
