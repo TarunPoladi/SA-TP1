d8bf098f5604fcd1df64fad91bd28c7e6560dc12
Bryan Oliveira <vicdaruf@yahoo.com>
2016-02-25 16:48:52 +0000

    Added SortFilter to SystemA folder

---
+++ System A/SortFilter.java
@@ -0,0 +1,100 @@
+/******************************************************************************************************************
+* File:MiddleFilter.java
+* Course: 17655
+* Project: Assignment 1
+* Copyright: Copyright (c) 2003 Carnegie Mellon University
+* Versions:
+*   1.0 November 2008 - Sample Pipe and Filter code (ajl).
+*
+* Description:
+*
+* This class serves as an example for how to use the FilterRemplate to create a standard filter. This particular
+* example is a simple "pass-through" filter that reads data from the filter's input port and writes data out the
+* filter's output port.
+*
+* Parameters:       None
+*
+* Internal Methods: None
+*
+******************************************************************************************************************/
+
+import java.util.*;                     // This class is used to interpret time words
+import java.text.SimpleDateFormat;      // This class is used to format and write time in a string format.
+
+public class SortFilter extends FilterFramework
+{
+    SortFilter(int input, int output) {
+		super(input, output);
+	}
+
+	public void run()
+    {
+        Calendar TimeStamp = Calendar.getInstance();
+        SimpleDateFormat TimeStampFormat = new SimpleDateFormat("yyyy MM dd::hh:mm:ss:SSS");
+
+        int IdLength = 4;               // This is the length of IDs in the byte stream
+
+        int bytesread = 0;                  // Number of bytes read from the input file.
+        int byteswritten = 0;               // Number of bytes written to the stream.
+        byte databyte = 0;                  // The byte of data read from the file
+
+        int id;                         // This is the measurement id
+
+        // Next we write a message to the terminal to let the world know we are alive...
+
+        System.out.print( "\n" + this.getName() + "::SortFilter Reading ");
+
+        while (true)
+        {
+            /*************************************************************
+            *   Here we read a byte and write a byte
+            *************************************************************/
+
+            try
+            {
+                id = 0;
+
+                for (i=0; i<IdLength; i++ )
+                {
+                    databyte = ReadFilterInputPort();   // This is where we read the byte from the stream...
+
+                    id = id | (databyte & 0xFF);        // We append the byte on to ID...
+
+                    if (i != IdLength-1)                // If this is not the last byte, then slide the
+                    {                                   // previously appended byte to the left by one byte
+                        id = id << 8;                   // to make room for the next byte we append to the ID
+
+                    } // if
+
+                    bytesread++;                        // Increment the byte count
+
+                } // for
+
+                if ( id == 0 )
+                {
+                    TimeStamp.setTimeInMillis(measurement);
+
+                } // if
+
+            } // try
+
+            catch (EndOfStreamException e)
+            {
+                ClosePorts();
+                System.out.print( "\n" + this.getName() + "::Middle Exiting; bytes read: " + bytesread + " bytes written: " + byteswritten );
+                break;
+
+            } // catch
+
+        } // while
+
+        // Sort
+
+
+        //Send
+        WriteFilterOutputPort(databyte);
+        byteswritten++;
+
+   } // run
+
+} // MiddleFilter
